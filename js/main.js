6f0f1aae30365eb8b93f55efb36c321ba9e40c66
Diego Gomes <diegomjua@gmail.com>
2023-02-07 13:35:48 -0300

    Github Pages

--- js/main.js
+++ js/main.js
@@ -3,18 +3,3 @@ import Kanban from "./view/Kanban.js";
 new Kanban(
     document.querySelector(".kanban")
 )
-
-function parallax_height() {
-    var scroll_top = $(this).scrollTop();
-    var sample_section_top = $(".sample-section").offset().top;
-    var header_height = $(".sample-header-section").outerHeight();
-    $(".sample-section").css({ "margin-top": header_height });
-    $(".sample-header").css({ height: header_height - scroll_top });
-}
-parallax_height();
-    $(window).scroll(function() {
-    parallax_height();
-  });
-    $(window).resize(function() {
-    parallax_height();
-  });
